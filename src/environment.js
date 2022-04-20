7fd349e413ac84b801102ce16124b6e6472778a3
Ron Wexler <ronmada@gmail.com>
2022-04-20 22:22:38 +0300

    api fix

--- src/environment.js
+++ src/environment.js
@@ -4,5 +4,5 @@ if (
   window.location.hostname === '127.0.0.1'
 )
   api = 'http://localhost:5000'
-else api = 'https://rest.chat-chain.com/'
+else api = 'https://rest.chat-chain.com'
 export { api }
